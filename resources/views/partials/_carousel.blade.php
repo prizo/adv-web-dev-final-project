8f9688f42934aaffcf893dfb20d6faa3fe2c483e
mr-robot9 <kheneahm9@yahoo.com>
2017-04-26 15:14:48 -0500

    Carousel and css updates

--- resources/views/partials/_carousel.blade.php
+++ resources/views/partials/_carousel.blade.php
@@ -1,17 +1,17 @@
 <!-- Carousel
 ================================================== -->
-<div id="myCarousel" style="width: 400px;height: 180px;" class="carousel slide">
-<!-- Carousel items -->
-<div class="carousel-inner">
- <div class="active item">
-   <div id="weather"></div> {{--shows weather--}}
- </div>
- <div class="item">
-   <div id="forecast"></div> {{--shows weather--}}
-</div>
-</div>
-<!-- Carousel nav -->
-<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+<div id="myCarousel" style="position: relative; top: 0px; right: 0px;" class="carousel slide">
+    <!-- Carousel items -->
+    <div class="carousel-inner" style="border-radius: 4px;">
+       <div class="active item">
+         <div id="weather" style="width: 311.66px; height: 180px;"></div> {{--shows weather--}}
+       </div>
+       <div class="item">
+         <div id="forecast" style="width: 311.66px; height: 180px;"></div> {{--shows weather--}}
+      </div>
+    </div>
+    <!-- Carousel nav -->
+    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
 </div>
 <!-- /.carousel -->
--- resources/views/workouts/index.blade.php
+++ resources/views/workouts/index.blade.php
@@ -1,69 +1,66 @@
 @extends('layouts.master')
 
-@section('title', '|  All Your Workouts')
+@section('title', '| All Workouts')
 
 @section('content')
+    @include('partials/_table')
+    <div class="row">
+      <div class="col-md-12">
+        <div class="panel panel-default">
+          <div class="panel-heading" style="font-weight: 600; color: #586069; padding: 4px 15px 4px 15px; height: 48px;">
+            <h4 style="margin-top: 3px;"><span style="font-weight: bold;">
+              Your workouts
+              <a href="{{route('workouts.create')}}" class="btn btn-success btn-workout" style="margin-left: 715px;">New workout</a>
+            </span></h4>
+          </div>
+            @if(count($workouts) <= 0)
+              <div class="panel-body">No workouts created...</div>
+            @else
+              <div class="panel-body" style="padding: 0px;">
+                <table class="table" style="margin-bottom: 0px;">
+                    {{--<thead>
+                      <th class="">Workout name</th>
+                      <th class="">Description</th>
+                      <th class="">Created on</th>
+                      <th class="col-md-2"></th>
+                    </thead>--}}
+                    <tbody>
+                      @foreach($workouts as $workout)
+                        <tr>
+                          <td>
+                            <span class="title-custom">{{$workout->title}}</span>
 
-  <div class="row">
-    <div class="col-md-10">
-      <h1>All Workouts</h1>
-    </div>
-    <div class="col-md-2">
-      <a href="{{route('workouts.create')}}" class="btn btn-lg btn-primary btn-h1-spacing"> Create New Workout</a>
-    </div>
-    <div class="col-md-12">
-      <hr />
-    </div>
-  </div> <!--end of row-->
-  <div class="row">
-    <div class="col-md-12">
-      @if(count($workouts) <= 0)
-        <h3>No Workouts created</h3>
-      @else
-      <table class="table">
-          <thead>
-            <th>#</th>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Created At</th>
-            <th></th>
-          </thead>
-          <tbody>
-            <?php $count = 1 ?>
-            @foreach($workouts as $workout)
-              <tr>
-                <th>
-                  {{$count}}
-                  <?php $count++ ?>
+                            {{-- only show a snippet of the body --}}
+                            {{substr($workout->description, 0, 50)}}
+                            {{-- ternary conditional? if true: if false --}}
+                            {{strlen($workout->description) > 50 ? '...': ""}}
 
-                </th>
-                <td>
-                  {{$workout->title}}
-                </td>
-                <td>
-                  {{-- only show a snippet of the body --}}
-                  {{substr($workout->description, 0, 50)}}
-                  {{-- ternary conditional? if true: if false --}}
-                  {{strlen($workout->description) > 50 ? '...': ""}}
-                </td>
-                <td>
-                  {{date('M j, Y h:i A', strtotime($workout->created_at))}}
-                </td>
-                <td>
-                  <a href="{{route('workouts.show', $workout->id)}}" class="btn btn-sm btn-default">View</a>
-                  <a href="{{route('workouts.edit', $workout->id)}}" class="btn btn-sm btn-default">Edit</a>
-
-                </td>
-              </tr>
-            @endforeach
-          </tbody>
-      </table>
-      <div class="text-center">
-        {!!$workouts->links()!!}
+                            <span style="font-weight: bold;">created on</span> {{date('M j, Y h:i A', strtotime($workout->created_at))}}
+                          </td>
+                          <td style="width: 205px;">
+                            <a href="{{route('workouts.show', $workout->id)}}" class="btn btn-default btn-sm" style="color: black; margin-left: 48px;">
+                              <span class="glyphicon glyphicon-eye-open"></span> View
+                            </a>
+                            <span>&nbsp</span>
+                            <a href="{{route('workouts.edit', $workout->id)}}" class="btn btn-default btn-sm" style="color: black;">
+                              <span class="glyphicon glyphicon-pencil"></span> Edit
+                            </a>
+                          </td>
+                        </tr>
+                      @endforeach
+                    </tbody>
+                </table>
+                <div class="text-center">
+                  {!!$workouts->links()!!}
+                </div>
+              </div>
+            @endif
+        </div>
       </div>
-    @endif
-
     </div>
-  </div>
-
+    {{--<div class="row">
+      <div class="container">
+        <a href="{{route('workouts.create')}}" class="btn btn-success btn-workout">Create workout</a>
+      </div>
+    </div>--}}
 @endsection
